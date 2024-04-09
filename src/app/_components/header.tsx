19a68653111d50c53a85161e2d311981772ac547
ftmhabt <ftmh.a78@gmail.com>
2024-04-09 22:32:35 +0330

    Fix image element

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Header() {
   return <nav className="flex items-center m-4 text-[.75rem] border border-gray-200 rounded-full">
     <div className="p-1  items-center flex gap-2">
-        <img className="w-[20px] h-[20px] rounded-full" src="https://avatars.githubusercontent.com/u/149063046?v=4" alt="my-photo" />
+        <Image 
+        className="rounded-full" 
+        src="https://avatars.githubusercontent.com/u/149063046?v=4" 
+        alt="my-photo" 
+        width='20'
+        height='20'/>
         <div>ftmhabt</div>
     </div>
     <ul className="flex ml-auto p-1 px-3 gap-5">
