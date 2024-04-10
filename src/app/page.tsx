c403abf16bde318d3aba51c5e51e9d60e3bca954
ftmhabt <ftmh.a78@gmail.com>
2024-04-10 22:03:32 +0330

    Fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./_components/hero";
 import WorksList from "./_components/works-list";
 
 export default function Home() {
-  return <div className="flex flex-col sm:flex-row sm:gap-[3rem] h-full sm:items-center justify-evenly">
+  return <div className="flex flex-col md:flex-row md:gap-[3rem] h-full md:items-center justify-evenly">
     <Hero/>
     <WorksList/>
   </div>;
