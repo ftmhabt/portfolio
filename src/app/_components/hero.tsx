8f579179bbc6e90660b486e599538c3f1897de0f
ftmhabt <ftmh.a78@gmail.com>
2024-04-10 21:59:14 +0330

    Add home page layout and style

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,8 +1,8 @@
 export default function Hero() {
   return (
-    <div className="flex flex-col py-10">
-      <div className="font-bold text-[2rem]">Nice to meet you!</div>
-      <div className="font-bold text-[2rem]">I&apos;m Fatemeh.</div>
+    <div className="flex flex-col py-10 sm:flex-1">
+      <div className="font-bold sm:text-[4rem] text-[2rem]">Nice to meet you!</div>
+      <div className="font-bold sm:text-[4rem] text-[2rem]">I&apos;m Fatemeh.</div>
       <div className="pt-5">
       Based in Iran, I&apos;m a web developer passionate about building a modern web application that users love.
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Hero from "./_components/hero";
+import WorksList from "./_components/works-list";
+
 export default function Home() {
-  return <main>
+  return <div className="flex flex-col sm:flex-row sm:gap-[3rem] h-full sm:items-center justify-evenly">
     <Hero/>
-  </main>;
+    <WorksList/>
+  </div>;
 }
