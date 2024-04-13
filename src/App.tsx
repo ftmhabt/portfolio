4949cdec07f2be1485a02fb5b64217d49f1a2cf1
ftmhabt <ftmh.a78@gmail.com>
2024-04-13 14:03:40 +0330

    Add select work logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import Header from "./components/header";
 import Hero from "./components/hero";
 import WorksList from "./components/works-list";
+import { useState } from "react";
+import Work from "./components/work";
 
 function App() {
+  const [work,setWork]:[work:string,setWork:(arg: string) => void]=useState('');
   return (
     <div className="max-w-[1024px] min-[1025px]:mx-auto mx-[2rem] h-[90vh]">
       <Header />
       <div className="flex flex-col md:flex-row md:gap-[3rem] h-full md:items-center justify-evenly">
         <Hero />
-        <WorksList />
+        <WorksList setWork={setWork} />
       </div>
+      <Work name={work}/>
     </div>
   );
 }
--- src/components/works-list.tsx
+++ src/components/works-list.tsx
@@ -1,13 +1,13 @@
-export default function WorksList() {
+export default function WorksList({setWork}:{setWork:(arg: string) => void}) {
   return (
     <div className="sm:flex-[.75] sm:pt-[2rem]">
       <div className="text-gray-400 py-5">selected projects</div>
       <ul className="flex flex-col justify-center *:pb-2 font-bold">
-        <li>e-commerse shop</li>
+        <li onClick={()=>setWork('shop')}>e-commerse shop</li>
         <hr />
-        <li>todo app</li>
+        <li onClick={()=>setWork('shop2')}>todo app</li>
         <hr />
-        <li>calculator</li>
+        <li onClick={()=>setWork('shop')}>calculator</li>
         <hr />
       </ul>
     </div>
