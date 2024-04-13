6f67b666d33669a348e0059450f72d4582f2b471
ftmhabt <ftmh.a78@gmail.com>
2024-04-13 12:47:02 +0330

    Delete all

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "./_components/hero";
-import WorksList from "./_components/works-list";
-
-export default function Home() {
-  return <div className="flex flex-col md:flex-row md:gap-[3rem] h-full md:items-center justify-evenly">
-    <Hero/>
-    <WorksList/>
-  </div>;
-}
