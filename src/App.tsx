ced98ab75f4e05c8145aa89adeec3d7b1c03437b
ftmhabt <ftmh.a78@gmail.com>
2024-04-13 13:09:17 +0330

    Update files to csr

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/header";
+import Hero from "./components/hero";
+import WorksList from "./components/works-list";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="max-w-[1024px] min-[1025px]:mx-auto mx-[2rem] h-[90vh]">
+      <Header />
+      <div className="flex flex-col md:flex-row md:gap-[3rem] h-full md:items-center justify-evenly">
+        <Hero />
+        <WorksList />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/works-list.tsx
@@ -0,0 +1,15 @@
+export default function WorksList() {
+  return (
+    <div className="sm:flex-[.75] sm:pt-[2rem]">
+      <div className="text-gray-400 py-5">selected projects</div>
+      <ul className="flex flex-col justify-center *:pb-2 font-bold">
+        <li>e-commerse shop</li>
+        <hr />
+        <li>todo app</li>
+        <hr />
+        <li>calculator</li>
+        <hr />
+      </ul>
+    </div>
+  );
+}
