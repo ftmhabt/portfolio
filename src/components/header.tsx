69bd550b711c756c28a6089725559ddf1dfe9cda
ftmhabt <ftmh.a78@gmail.com>
2024-04-20 19:06:29 +0330

    Change theme colors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <nav className="fixed left-1/2 -translate-x-1/2 z-10 flex items-center text-[.75rem] border border-gray-100 rounded-full top-[1rem] w-[90vw] bg-white shadow-[0_8px_30px_rgb(0,0,0,0.04)] sm:border-0 sm:shadow-[0_0_0_rgb(0,0,0,0)] sm:bg-transparent">
-      <div className="p-1  items-center flex gap-2">
+    <nav className="fixed left-1/2 -translate-x-1/2 z-10 flex items-center text-[.75rem] rounded-full top-[1rem] w-[90vw] bg-[#F3F2EE] shadow-[0_8px_30px_rgb(0,0,0,0.04)] sm:shadow-[0_0_0_rgb(0,0,0,0)] bg-transparent">
+      <div className="p-1 items-center flex gap-2">
         <img
           className="rounded-full"
           src="https://avatars.githubusercontent.com/u/149063046?v=4"
@@ -11,7 +11,7 @@ export default function Header() {
         />
         <div>ftmhabt</div>
       </div>
-      <ul className="flex ml-auto p-1 px-3 gap-5 rounded-full sm:bg-white/50 sm:backdrop-blur-sm">
+      <ul className="flex ml-auto p-1 px-3 gap-5 rounded-full bg-[#F3F2EE]/50 backdrop-blur-sm">
         <li>About</li>
         <li>Contact</li>
       </ul>
