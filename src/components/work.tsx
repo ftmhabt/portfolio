295a62516517cf1dbda7940225f69a209bede21f
ftmhabt <ftmh.a78@gmail.com>
2024-04-22 16:24:55 +0330

    Improve styles

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,5 +1,7 @@
 import works from "./works-data";
 import { useEffect } from "react";
+import Icon from '@mdi/react';
+import { mdiStarFourPointsSmall,mdiOpenInNew } from '@mdi/js';
 
 export default function Work({
   workId,
@@ -24,7 +26,7 @@ export default function Work({
           isAnimating && "-translate-y-24"
         }  duration-300 w-screen bg-[#1d1d1d] text-[#f3f2ee] p-[2rem] top-full left-0 flex flex-col gap-[1rem]`}
       >
-        <div className="text-2xl flex justify-between">
+        <div className="text-3xl flex justify-between">
           <div>{work.name}</div>
           <button
           className="sm:hidden block"
@@ -37,15 +39,17 @@ export default function Work({
         </div>
         <div className="flex flex-col sm:flex-row gap-[1rem]">
           <div className="flex flex-col gap-3">
-            <div className="flex gap-3">
-              <a href={work.liveUrl}>live demo</a>
-              <a href={work.repoUrl}>repo</a>
+            <div className="flex gap-2 even:*:mr-3">
+              <a href={work.liveUrl} target="_blank">live demo</a>
+              <Icon path={mdiOpenInNew} color='#f3f2ee' size={0.75} className="self-center"/>
+              <a href={work.repoUrl} target="_blank">repo</a>
+              <Icon path={mdiOpenInNew} color='#f3f2ee' size={0.75} className="self-center"/>
             </div>
             <div>{work.description}</div>
             <div className="text-2xl">technologies, tools, libraries</div>
             <ul>
               {work.tools.map((item) => (
-                <li key={item}>{item}</li>
+                <li key={item} className="flex gap-2 pb-1"><Icon path={mdiStarFourPointsSmall} color='#f3f2ee' size={1} />{item}</li>
               ))}
             </ul>
           </div>
