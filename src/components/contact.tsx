1bce424bd3bd551554c26e0ddd64aa08b8b07193
ftmhabt <ftmh.a78@gmail.com>
2024-04-22 15:25:17 +0330

    Add contact me content

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,5 +1,15 @@
+import Icon from '@mdi/react';
+import { mdiAt,mdiPhone,mdiMapMarker,mdiLinkedin } from '@mdi/js';
+
+
 export default function Contact() {
   return (
-    <div>contact</div>
+    <div className='flex flex-col gap-5 *:flex *:gap-2 *:border *:border-[#1d1d1d] *:w-fit first:*:border-0 *:p-2 *:pr-4 first:*:p-0 *:rounded-md'>
+      <div>Interested in working together? Let's chat!</div>
+      <div><Icon path={mdiAt} size={1} />ftmh.a78@gmail.com</div>
+      <div><Icon path={mdiPhone} size={1} />+98 922 9688 704</div>
+      <div><Icon path={mdiMapMarker} size={1} />Qom, Iran</div>
+      <div><Icon path={mdiLinkedin} size={1} /><a href="https://www.linkedin.com/in/fatemeh-abtahi/">Fatemeh Abtahi</a></div>
+    </div>
   )
 }
