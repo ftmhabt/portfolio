0cda0267183d4c5b32c5363f43f92c1c297b8f7c
ftmhabt <ftmh.a78@gmail.com>
2024-05-16 20:31:22 +0330

    Render works list from data

--- src/components/works-list.tsx
+++ src/components/works-list.tsx
@@ -1,29 +1,25 @@
+import works from "./works-data";
 export default function WorksList({
   setWorkId,
-  setIsAnimating
+  setIsAnimating,
 }: {
   setWorkId: (arg: number | null) => void;
   setIsAnimating: (arg: boolean) => void;
 }) {
-  const handleClick = (id:number) => {
+  const handleClick = (id: number) => {
     setWorkId(id);
-    setIsAnimating(false); 
+    setIsAnimating(false);
   };
   return (
     <div className="sm:flex-[.75] sm:pt-[2rem]">
       <div className="text-gray-400 py-5">selected projects</div>
       <ul className="flex flex-col justify-center *:pb-2 font-bold">
-        <li onClick={() => handleClick(1)}>
-          e-commerse shop
-        </li>
-        <hr />
-        <li onClick={() => handleClick(2)}>
-          todo app
-        </li>
-        <hr />
-        <li onClick={() => handleClick(3)}>
-          calculator
-        </li>
+        {works.map((work) => (
+          <>
+            <li onClick={() => handleClick(work.id)}>{work.name}</li>
+            <hr />
+          </>
+        ))}
         <hr />
       </ul>
     </div>
